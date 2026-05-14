0412f79c9d8b5b6f02de6f1108cf902132e54fcf
Aleksander Kurgan <113726847+Rand0m0User1@users.noreply.github.com>
2026-05-13 22:20:36 -0400

    Update wsgi.py for deployment

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
+from werkzeug.middleware.proxy_fix import ProxyFix
 from app import app
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_prefix=1, x_proto=1, x_host=1)
 
 if __name__ == "__main__":
     app.run()
-
